8ec5a618b797baf8530718549ea0d1617eb052d9
Dave Marsh <davemarsh77@gmail.com>
2024-08-16 14:40:36 -0400

    restore recording

--- web/record_control_handler.go
+++ web/record_control_handler.go
@@ -3,18 +3,31 @@ package web
 import (
 	"log"
 	"net/http"
+	"time"
 	"v4lvid/video"
 )
 
 var _ http.Handler = (*RecordControlHandler)(nil)
 
 type RecordControlHandler struct {
-	Server *video.Server
-	Url    string
-	Icon   string
+	Server    *video.Server
+	Url       string
+	Icon      string
+	recording bool
 }
 
 func (ctl *RecordControlHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	log.Printf("recording...")
-	ctl.Server.RecordCmd(60)
+	if !ctl.recording {
+		log.Printf("recording...")
+		ctl.recording = true
+		ctl.Server.RecordCmd(60)
+		time.AfterFunc(60*time.Second, func() {
+			log.Println("timer finished", ctl.recording)
+			ctl.recording = false
+		})
+
+	} else {
+		ctl.Server.StopRecordCmd()
+		ctl.recording = false
+	}
 }
